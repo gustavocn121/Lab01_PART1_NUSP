4e479c07260f5ca31750763f7fff3d2511096649
gustavocn121 <gustavo.cn121@gmail.com>
2026-03-21 14:42:37 -0300

    refactor: restructure config.yaml and update main.py to read configuration

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 from src.ingestion.job import run as ingestion_run
+from src.processing.job import run as processing_run
+from src.utils import read_config
+import os
 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def main():
-    ingestion_run()
+    config = read_config(f"{BASE_DIR}/config.yaml")
+    ingestion_run(config)
+    processing_run(config)
 
 
 if __name__ == "__main__":
